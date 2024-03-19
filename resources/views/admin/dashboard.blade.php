ae6aa65a88910509a8c1b2b62d1b1f8ea4b606c8
LordSnow <jeantshimbalanga9@gmail.com>
2024-03-19 11:51:46 +0100

    Adding the asset function to the hmtl file

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1062,21 +1062,21 @@ License: For each use you must have a valid license purchased only from above li
 	</div>
 
 	<!-- core:js -->
-	<script src="../assets/vendors/core/core.js"></script>
+	<script src="{{asset('../assets/vendors/core/core.js')}}"></script>
 	<!-- endinject -->
 
 	<!-- Plugin js for this page -->
-  <script src="../assets/vendors/flatpickr/flatpickr.min.js"></script>
-  <script src="../assets/vendors/apexcharts/apexcharts.min.js"></script>
+  <script src="{{asset('../assets/vendors/flatpickr/flatpickr.min.js')}}"></script>
+  <script src="{{asset('../assets/vendors/apexcharts/apexcharts.min.js')}}"></script>
 	<!-- End plugin js for this page -->
 
 	<!-- inject:js -->
-	<script src="../assets/vendors/feather-icons/feather.min.js"></script>
-	<script src="../assets/js/template.js"></script>
+	<script src="{{asset('../assets/vendors/feather-icons/feather.min.js')}}"></script>
+	<script src="{{asset('../assets/js/template.js')}}"></script>
 	<!-- endinject -->
 
 	<!-- Custom js for this page -->
-  <script src="../assets/js/dashboard-dark.js"></script>
+  <script src="{{asset('../assets/js/dashboard-dark.js')}}"></script>
 	<!-- End custom js for this page -->
 
 </body>
