9451879d0141b8ae5e929ec5b8f412d56d6595d1
LordSnow <jeantshimbalanga9@gmail.com>
2024-03-20 22:04:56 +0100

    Configuring the Admin auth

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -42,7 +42,28 @@ class AuthenticatedSessionController extends Controller
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
-
-        return redirect('/');
+                    
+        return redirect('/login');
+        
+        
     }
+
+    public function AdminLogin()
+    {
+        return view('admin.login');
+    }
+
+    public function Admindestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+                    
+        return redirect('/admin/login');
+        
+        
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,13 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    if(Auth::user()->role=="admin"){
+        return view("admin.index");
+    }
+    else{
+        return view('dashboard');
+    }
+    
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -36,9 +43,11 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
 
     Route::get("/admin/dashboard", [AdminController::class, 'AdminDashboard'])->name("admin.dashboard");
     Route::get("/admin/dashboard/email/inbox", [AdminController::class, 'AdminDashboardInbox'])->name("admin.dashboard.inbox");
-    Route::get("/admin/dashboard/logout", [AuthenticatedSessionController::class, 'destroy'])->name("admin.logout");
+    Route::get("/admin/logout", [AuthenticatedSessionController::class, 'Admindestroy'])->name("admin.logout");
 
 });
+
+Route::get("/admin/login", [AuthenticatedSessionController::class, 'AdminLogin'])->name("admin.login");
 
 Route::middleware(['auth', 'role:agent'])->group(function(){
     Route::get("/agent/dashboard", [AgentController::class, 'AgentDashboard'])->name("agent.dashboard");
